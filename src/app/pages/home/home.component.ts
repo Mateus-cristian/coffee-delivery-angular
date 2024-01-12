6e3441dafb32299c584367f88c03cb45107ec5ba
mateus-cristian <mateus_cristian@outlook.com>
2024-01-12 20:55:43 -0300

    feat:call getProducts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct, IProducts } from 'src/app/interfaces/Product';
+import { ProductsService } from 'src/app/services/products.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  coffees: IProduct[] = [];
 
-  constructor() { }
+  baseApiUrl = environment.baseApiUrl;
+
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
-  }
+    this.productsService.getProducts().subscribe((response) => {
+      const data = response;
 
+      this.coffees = data;
+    });
+  }
 }
