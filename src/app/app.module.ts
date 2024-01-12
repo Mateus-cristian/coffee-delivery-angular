280b10505c92f9a0e73edc2853152b3e5219e3fe
mateus-cristian <mateus_cristian@outlook.com>
2024-01-12 20:52:19 -0300

    feat:add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -12,8 +13,22 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { DeliveryComponent } from './pages/delivery/delivery.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, CardCoffeeComponent, CheckoutComponent, DeliveryComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardCoffeeComponent,
+    CheckoutComponent,
+    DeliveryComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
