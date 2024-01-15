30f176ce48055ee3f5fb7819e00be0754d158623
mateus-cristian <mateus_cristian@outlook.com>
2024-01-15 13:27:51 -0300

    fix:styles,store functions

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { faCartShopping  } from '@fortawesome/free-solid-svg-icons';
-import { faTag  } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IProduct } from 'src/app/interfaces/Product';
+import { CartModel, IAppState } from 'src/app/store/cart/cart.model';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +16,27 @@ export class HeaderComponent implements OnInit {
   faCartShopping = faCartShopping;
   faTag = faTag;
 
-  constructor() {}
+  cart$: Observable<CartModel>;
 
-  ngOnInit(): void {}
+  products: IProduct[] = [];
+
+  constructor(private store: Store<IAppState>, private router: Router) {
+    this.cart$ = this.store.select((state: IAppState) => state.cart);
+  }
+
+  ngOnInit(): void {
+    this.cart$.subscribe((cart: CartModel) => {
+      const products = cart.cart;
+
+      this.products = products;
+    });
+  }
+
+  navigateToCartCheckout(): void {
+    this.router.navigate(['checkout']);
+  }
+
+  navigateToHome(): void {
+    this.router.navigate(['']);
+  }
 }
