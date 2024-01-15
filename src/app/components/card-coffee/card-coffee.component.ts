677bfd900aeedc965330326b8747021499443fc5
mateus-cristian <mateus_cristian@outlook.com>
2024-01-15 13:27:42 -0300

    fix:styles,store functions

--- src/app/components/card-coffee/card-coffee.component.ts
+++ src/app/components/card-coffee/card-coffee.component.ts
@@ -1,7 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+// card-coffee.component.ts
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Store } from '@ngrx/store';
 import { IProduct } from 'src/app/interfaces/Product';
+import { addProductToCart } from 'src/app/store/cart/cart.action';
+
+
+import { CartModel } from 'src/app/store/cart/cart.model';
 
 @Component({
   selector: 'app-card-coffee',
@@ -12,8 +18,12 @@ export class CardCoffeeComponent implements OnInit {
   faCartShopping = faCartShopping;
 
   @Input() coffee: IProduct = {} as IProduct;
+  @Output() eventUpdateCountToProduct = new EventEmitter();
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(
+    private sanitizer: DomSanitizer,
+    private store: Store<CartModel>
+  ) {}
 
   ngOnInit(): void {
     const { price, ...coffee } = this.coffee;
@@ -25,5 +35,17 @@ export class CardCoffeeComponent implements OnInit {
 
   getImageURL(imagePath: string) {
     return this.sanitizer.bypassSecurityTrustUrl(imagePath);
+  }
+
+  async addCountToProduct(product: IProduct) {
+    this.eventUpdateCountToProduct.emit({ product: product, type: 'add' });
+  }
+
+  async removeCountToProduct(product: IProduct) {
+    this.eventUpdateCountToProduct.emit({ product: product, type: 'remove' });
+  }
+
+  async add(product: IProduct) {
+    this.store.dispatch(addProductToCart({ product }));
   }
 }
